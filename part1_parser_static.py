b5915a5fbdebed732458a1c0de0618a8e9c212b0
Petr Dumanskij <dumanskij@MacBook-Air-Petr.local>
2021-04-07 15:59:59 +0300

    Дополнен файл part1_parser_static.py

--- part1_parser_static.py
+++ part1_parser_static.py
@@ -1,5 +1,4 @@
 # Статический парсер котировок
-# Дневой фрейм
 
 # pip3 install pandas_datareader
 # pip3 install datetime
@@ -10,33 +9,86 @@ import datetime as dt
 import yfinance as yf
 import json
 
+# Тикер
+ticker = 'SBER.ME'
+
+# Таймфрейм
+timeframe = '5m'
+
 # Временные рамки для получения котировок
-start = dt.datetime(2021, 4, 1)
+start = dt.datetime(2021, 3, 1)
 end = dt.datetime.now()
 
-# Таблица котировок указанной ценной бумаги за период
+
+# Таблица котировок указанной ценной бумаги за период, таймфрейм 1d
 # Формат: Date, Adj Close, Close, High, Low, Open, Volume
-# Котировки Сбербанка как пример
-try:
-    data = web.DataReader(["SBER.ME"], "yahoo", start, end)
-except Exception as err:
-    print(f'При получении списка котировок возникла ошибка: {err}')
+# Получить список котировок тикера за указанный период
+def get_quotes(_ticker, _start, _end):
+    try:
+        data = web.DataReader([_ticker], "yahoo", _start, _end)
+    except Exception as err:
+        print(f'При получении списка котировок возникла ошибка: {err}')
+    else:
+        quotes = []
+        # Фильтр нужных категорий
+        data_lite = data.drop(['Adj Close', 'High', 'Low', 'Volume'], 1)
+        # Котировки открытия
+        open_data = data_lite[('Open', _ticker)].values
+        # Котировки закрытия
+        close_data = data_lite[('Close', _ticker)].values
 
-# Вывод цен закрытия и открытия
-print(data.drop(['Adj Close', 'High', 'Low', 'Volume'], 1))
+        # Добавление котировок в список quotes
+        for price in range(len(open_data)):
+            quotes.append((open_data[price], close_data[price]))
+            # quotes.append(open_data[price])
+            # quotes.append(close_data[price])
 
-# Преобразование в json формат
-data_lite = data.drop(['Adj Close', 'High', 'Low', 'Volume'], 1)[:5]
-data_lite_json = data_lite.to_json(indent=2)
-print(data_lite_json)
+        return json.dumps(quotes, indent=2)  # json формат
+    return None
+
+
+# список списков [цена открытия, цена закрытия]
+get_quotes(ticker, start, end)
 
 
 # Таблица котировок yahoo указанной ценной бумаги за период
+# с возможностью выбора интервала
 # Формат: Date, Open, High, Low, Close, Adj Close, Volume
-# Котировки AAPL, таймфрейм 5 минут
+def get_quotesY(_ticker, _start, _end, _timeframe):
+    try:
+        data = yf.download(_ticker, _start, _end, interval=_timeframe)
+    except Exception as err:
+        print(f'При получении списка котировок возникла ошибка: {err}')
+    else:
+        quotesY = []
+        # Фильтр нужных категорий
+        data_lite = data.drop(['Adj Close', 'High', 'Low', 'Volume'], 1)
+        # Котировки открытия
+        open_data = data_lite['Open'].values
+        # Котировки закрытия
+        close_data = data_lite['Close'].values
+
+        # Добавление котировок в список quotes
+        for price in range(len(open_data)):
+            quotesY.append((open_data[price], close_data[price]))
+            # quotes.append(open_data[price])
+            # quotes.append(close_data[price])
+
+        return json.dumps(quotesY, indent=2)  # json формат
+    return None
+
+
+# список списков [цена открытия, цена закрытия]
+get_quotesY(ticker, start, end, timeframe)
+
+
+# Таблица котировок yahoo указанной ценной бумаги за период
+# с возможностью выбора интервала
+# Формат: Date, Open, High, Low, Close, Adj Close, Volume
+# Котировки AAPL, таймфрейм 10m
 try:
-    dataY = yf.download('AAPL', start, end, interval="5m")
+    dataY = yf.download('AAPL', start, end, interval="15m")
 except Exception as err:
     print(f'При получении списка котировок возникла ошибка: {err}')
-
-print(dataY)
+else:
+    print(dataY)
