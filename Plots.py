6c88178727c4373e5be64f99292fad805e3cbf0c
Artem <dzbarts>
2021-05-21 21:06:09 +0300

    график MACD

--- Plots.py
+++ Plots.py
@@ -1,19 +1,18 @@
 import Strategy
 import matplotlib.pyplot as plt
 import Indicators
-import json
 import Data
 import datetime as dt
 
 
-
-def plot_SMA(ticker, start, end,  window):
+def plot_SMA(ticker, start, end, window):
     data = Data.getAllQuotes(ticker, start, end)
     data = data['Close']
     sma = Indicators.SMA(data, window)
     plt.plot(data)
     plt.plot(sma)
     plt.show()
+
 
 def plot_twoSMA(ticker, start, end):
     data = Data.getAllQuotes(ticker, start, end)
@@ -26,7 +25,7 @@ def plot_twoSMA(ticker, start, end):
     plt.show()
 
 
-def plot_EMA(ticker, start, end,  window):
+def plot_EMA(ticker, start, end, window):
     data = Data.getAllQuotes(ticker, start, end)
     data = data['Close']
     ema = Indicators.SMA(data, window)
@@ -35,34 +34,23 @@ def plot_EMA(ticker, start, end,  window):
     plt.show()
 
 
-plot_EMA('SBER.ME', dt.datetime(2019, 1, 1), dt.datetime(2021, 5, 1),100)
+plot_EMA('SBER.ME', dt.datetime(2019, 1, 1), dt.datetime(2021, 5, 1), 100)
 
 
+def plot_MACD(ticker, start, end, shortwindow, longwindow, signalwindow):
+    data = Data.getAllQuotes(ticker, start, end)
+    macd = Indicators.MACD(data['Close'], shortwindow, longwindow, signalwindow)
 
+    plt.bar(data['Date'], macd[0])
+    plt.bar(data['Date'], macd[1])
+    plt.bar(data['Date'], macd[2])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    plt.show()
 
 
 import pandas_datareader as web
 import datetime as dt
-import json
-import pandas as pd
-import plotly
 import plotly.graph_objs as go
-
 
 ticker = 'TSLA'
 
@@ -73,16 +61,13 @@ df = web.DataReader(ticker, "yahoo", start, end)
 
 df = df.reset_index()
 
-
-fig = go.Figure(data = [go.Candlestick(
-    x = df['Date'],
-    open = df['Open'],
-    high = df['High'],
-    low = df['Low'],
-    close = df['Adj Close']
+fig = go.Figure(data=[go.Candlestick(
+    x=df['Date'],
+    open=df['Open'],
+    high=df['High'],
+    low=df['Low'],
+    close=df['Adj Close']
 
 )])
 
-#fig.show()
-
-
+# fig.show()
