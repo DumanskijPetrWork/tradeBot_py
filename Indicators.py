ae29794af2ec6e5fa64d94857a3d39d6545ab1b0
Artem <dzbarts>
2021-05-22 17:50:11 +0300

    индикатор RSI

--- Indicators.py
+++ Indicators.py
@@ -80,3 +80,36 @@ def MACD(data, shortperiod, longperiod, signalperiod):
     return macd, macdsignal, macdhist
 
 
+# Relative strength index
+# https://en.wikipedia.org/wiki/Relative_strength_index
+def RSI(data, period):
+    u_days = []
+    d_days = []
+
+    for i, _ in enumerate(data):
+        if i == 0:
+            u_days.append(0)
+            d_days.append(0)
+        else:
+            if data[i] > data[i-1] :
+                u_days.append(data[i] - data[i-1])
+                d_days.append(0)
+            elif data[i] < data[i-1]:
+                d_days.append(data[i-1] - data[i])
+                u_days.append(0)
+            else:
+                u_days.append(0)
+                d_days.append(0)
+
+    ema_u = EMA(u_days, period)
+    ema_d = EMA(d_days, period)
+
+    result = []
+
+    for k, _ in enumerate(data):
+        if ema_d[k] == 0:
+            result.append(100)
+        else:
+            result.append(100 - (100 / (1 + ema_u[k]/ema_d[k])))
+
+    return result
